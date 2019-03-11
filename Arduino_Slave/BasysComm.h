9da5f9d854a88695f72a0e763c90b95a00edad76
jctenav <47014822+jctenav@users.noreply.github.com>
2019-03-10 23:28:04 -0700

    Add files via upload

---
+++ Arduino_Slave/BasysComm.h
@@ -0,0 +1,91 @@
+// ------------------------------------------------------------
+// Author: Julio Tena
+// Descrition: Library to establish communication between Basys3
+//             board and Arduino. Basys3 sends 8-bits to Arduino
+//             to represent data for 9g Servo motor control.
+//
+//
+// ------------------------------------------------------
+
+
+
+
+/*
+    Reads inputs from 8 different pins (pins 2 - 9)
+*/
+void readPorts(int basys_inputs[8])
+{
+  // 4-bits to determine location of Servo
+  // Range of location: 0 - 12 (decimal)
+  basys_inputs[0] = digitalRead(2);
+  basys_inputs[1] = digitalRead(3);
+  basys_inputs[2] = digitalRead(4);
+  basys_inputs[3] = digitalRead(5);
+
+  // 4-bits to determine voltage readings from xadc
+  // Range of voltage levels: 0 - 16 (decimal)
+  basys_inputs[4] = digitalRead(6);
+  basys_inputs[5] = digitalRead(7);
+  basys_inputs[6] = digitalRead(8);
+  basys_inputs[7] = digitalRead(9);
+}
+
+void sweep(int basys_input[8], int &pos_in)
+{
+
+
+  //location 0:
+  if (basys_input[0] == 0 && basys_input[1] == 0 && basys_input[2] == 0 && basys_input[3] == 0)
+    pos_in = pos_in;
+
+  //location 1:
+  if (basys_input[0] == 1 && basys_input[1] == 0 && basys_input[2] == 0 && basys_input[3] == 0)
+    pos_in = 15;
+
+  //location 2:
+  if (basys_input[0] == 0 && basys_input[1] == 1 && basys_input[2] == 0 && basys_input[3] == 0)
+    pos_in = 30;
+
+  //location 3:
+  if (basys_input[0] == 1 && basys_input[1] == 1 && basys_input[2] == 0 && basys_input[3] == 0)
+    pos_in = 45;
+
+  //location 4:
+  if (basys_input[0] == 0 && basys_input[1] == 0 && basys_input[2] == 1 && basys_input[3] == 0)
+    pos_in = 60;
+
+  //location 5:
+  if (basys_input[0] == 1 && basys_input[1] == 0 && basys_input[2] == 1 && basys_input[3] == 0)
+    pos_in = 75;
+
+
+  //location 6:
+  if (basys_input[0] == 0 && basys_input[1] == 1 && basys_input[2] == 1 && basys_input[3] == 0)
+    pos_in = 90;
+
+  //location 7:
+  if (basys_input[0] == 1 && basys_input[1] == 1 && basys_input[2] == 1 && basys_input[3] == 0)
+    pos_in = 105;
+
+  //location 8:
+  if (basys_input[0] == 0 && basys_input[1] == 0 && basys_input[2] == 0 && basys_input[3] == 1)
+    pos_in = 120;
+
+  //location 9:
+  if (basys_input[0] == 1 && basys_input[1] == 0 && basys_input[2] == 0 && basys_input[3] == 1)
+    pos_in = 135;
+
+  //location 10:
+  if (basys_input[0] == 0 && basys_input[1] == 1 && basys_input[2] == 0 && basys_input[3] == 1)
+    pos_in = 150;
+
+  //location 11:
+  if (basys_input[0] == 1 && basys_input[1] == 1 && basys_input[2] == 0 && basys_input[3] == 1)
+    pos_in = 165;
+
+  //location 12:
+  if (basys_input[0] == 0 && basys_input[1] == 0 && basys_input[2] == 1 && basys_input[3] == 1)
+    pos_in = 180;
+
+
+}
